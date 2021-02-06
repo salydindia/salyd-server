fa8cf62b64650dd2d86da1d52db9ff4f127896cd
kg-kartik <goel.kartik39@gmail.com>
2021-02-07 02:35:03 +0530

    Basic graphql setup with express and added logger

---
+++ middleware/requireLogin.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const Users = require("../src/users/users.models.js");
+require("dotenv").config();
+
+module.exports = async (request) => {
+    const { authorization } = request.req.headers;
+
+    const secret = process.env.JWT_SECRET;
+    if (!authorization) {
+        return {
+            message: "Authorization header not present",
+            isAuth: false,
+        };
+    }
+
+    //Getting token from the authorization bearer
+    const token = authorization.replace("Bearer ", "");
+
+    try {
+        //Verifying token for accessing protected pages
+        const user = jwt.verify(token, secret);
+        const { _id } = user;
+        const userData = await Users.findById({
+            _id,
+        });
+
+        return {
+            isAuth: true,
+            userData,
+            message: "Authenticated",
+        };
+    } catch (e) {
+        return {
+            isAuth: false,
+            message: "Not authenticated to access this resouce",
+        };
+    }
+};
---
+++ server.js
@@ -0,0 +1,69 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const schema = require("./src/schema");
+const bodyparser = require("body-parser");
+const morgan = require("morgan");
+const logger = require("./config/winston");
+const requireLogin = require("./middleware/requireLogin");
+
+dotenv.config({
+    path: ".env",
+});
+
+const uri = process.env.MONGO_URI;
+
+const app = express();
+
+const options = {
+    keepAlive: 1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+mongoose
+    .connect(uri, options)
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const server = new ApolloServer({
+    schema,
+    context: requireLogin,
+    logger,
+    tracing: true,
+    plugins: [
+        {
+            requestDidStart() {
+                return {
+                    didEncounterErrors(requestContext) {
+                        requestContext.logger.error(requestContext.errors);
+                    },
+                };
+            },
+        },
+    ],
+});
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(morgan("combined", { stream: logger.stream }));
+
+//Applying middlewares like body parser and cors
+server.applyMiddleware({
+    app,
+    path: "/api",
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    logger.info(
+        `Apollo server listening at http://localhost:5000${server.graphqlPath}`
+    );
+});
